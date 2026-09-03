ebe2985d9825eb1a3bc170720cfdbdafb5f473d2
zhaoqiao1208 <1203414064@qq.com>
2026-09-03 12:20:26 +0800

    fix: move skip condition after memory loading (was referencing undefined memory_summary)

--- gateway.py
+++ gateway.py
@@ -5028,9 +5028,6 @@ class GatewayService:
         today_logs = [e for e in log_entries if (e.get("time") or "").startswith(today_str)]
         fp_entries = self._load_footprints(100)
         today_fps = [e for e in fp_entries if (e.get("time") or "").startswith(today_str)]
-        if not today_logs and not today_fps and not memory_summary:
-            logger.info("Diary skip | no data for %s", today_str)
-            return
         log_summary = ""
         for e in today_logs:
             log_summary += f"[{e.get('time','')}] {e.get('location','')} - {e.get('content','')}\n"
